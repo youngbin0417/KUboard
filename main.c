c084877ac580b1b22b11bb08b530a13085e3ea3e
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2021-05-25 15:12:21 +0900

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,13 @@ bool correct(const char* que) {
 	}
 }
 
+void score_save(const char* name, const int score) {
+	FILE* fp = fopen("점수.txt", "a");
+	fputs(name, fp);
+	fprintf(fp, " %d", score);
+	fputs("\n", fp);
+}
+
 int main() {
 	printf("hello world");
 
