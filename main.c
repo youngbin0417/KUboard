1720018b81b1e1b0eb593bd338d92841deed3584
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2021-05-25 14:26:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#define MAX 30;
+
+bool correct(const char* que) {
+	char arr[MAX];
+	gets_s(arr, MAX);
+	if (strcmp(const char* que, const char* arr) == 0) {
+		return 1; // 두문자가 일치할 때
+	}
+	else
+		return 0;
+}
 
 int main() {
 	printf("hello world");
